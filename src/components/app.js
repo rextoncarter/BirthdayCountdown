dec0e54ca6a36826e8d949fe5c1a3769e87e304d
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-30 11:06:00 -0600

    pushing for use on laptop

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Birthday Countdown</h1>
+      <div className="grid">
+      <div className="grid__skew-dark-one-box" > </div>
+       <div className="grid__skew-dark-two" > </div>
+        <div className="grid__skew-dark-three" > </div>
+       
+        <div className="grid__skew-light-one">  </div>
+         <div className="grid__skew-light-two">  </div>
+          <div className="grid__skew-light-three-box">  </div>
       </div>
     );
   }
